9168e5c9c91f25b2233d28f87ae71fd160080b1d
Kostya Kryukaev <kryukaev.kostya@yandex.ru>
2024-11-25 20:56:38 +0300

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+	padding: 20px;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Div>
+			<i
+				className="fa fa-spotify"
+				style={{ fontSize: '48px' }}
+				aria-hidden="true"
+			></i>
+			<h2>Hello world</h2>
+		</Div>
+	);
 }
 
 export default App;
