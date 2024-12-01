7dfdd41ce415dc274a28a203bd60603bd9d41c07
Kostya Kryukaev <kryukaev.kostya@yandex.ru>
2024-12-01 15:25:31 +0300

    Реализовать футер

--- src/blog.js
+++ src/blog.js
@@ -1,6 +1,6 @@
 //json-server --watch src/db.json --port 3005
 import { Route, Routes } from 'react-router-dom';
-import { Header } from './components';
+import { Header, Footer } from './components';
 import styled from 'styled-components';
 
 const AppColumn = styled.div`
@@ -20,8 +20,6 @@ const Content = styled.div`
 const H2 = styled.h2`
 	text-align: center;
 `;
-
-const Footer = () => <div>Футер</div>;
 
 export const Blog = () => {
 	return (
